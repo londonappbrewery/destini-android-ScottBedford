8d1d50d3ab6b54425273494efc0d1b63c7150b23
Scott Bedford <bidfid@gmail.com>
2017-06-12 19:53:39 +1000

    Completed Challenge.

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -2,11 +2,22 @@ package com.londonappbrewery.destini;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import static com.londonappbrewery.destini.R.string.T1_Ans1;
+import static com.londonappbrewery.destini.R.string.T2_Ans1;
 
 public class MainActivity extends AppCompatActivity {
 
     // TODO: Steps 4 & 8 - Declare member variables here:
-
+    public TextView mStoryTextView;
+    public Button mButtonTop;
+    public Button mButtonBottom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,16 +26,51 @@ public class MainActivity extends AppCompatActivity {
 
 
         // TODO: Step 5 - Wire up the 3 views from the layout to the member variables:
-
-
+        mStoryTextView = (TextView) findViewById(R.id.storyTextView);
+        mButtonTop = (Button) findViewById(R.id.buttonTop);
+        mButtonBottom = (Button) findViewById(R.id.buttonBottom);
 
         // TODO: Steps 6, 7, & 9 - Set a listener on the top button:
-
-
-
+        mButtonTop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mButtonTop.getText() == getResources().getString(R.string.T1_Ans1)){
+                    mButtonTop.setText(getResources().getString(R.string.T3_Ans1));
+                    mButtonBottom.setText(getResources().getString(R.string.T3_Ans2));
+                    mStoryTextView.setText(getResources().getString(R.string.T3_Story));
+                } else if (mButtonTop.getText() == getResources().getString(R.string.T2_Ans1)) {
+                    mButtonTop.setText(getResources().getString(R.string.T3_Ans1));
+                    mButtonBottom.setText(getResources().getString(R.string.T3_Ans2));
+                    mStoryTextView.setText(getResources().getString(R.string.T3_Story));
+                } else if (mButtonTop.getText() == getResources().getString(R.string.T3_Ans1)) {
+                    mStoryTextView.setText(getResources().getString(R.string.T6_End));
+                    hideButtons();
+                }
+            }
+        });
 
         // TODO: Steps 6, 7, & 9 - Set a listener on the bottom button:
+        mButtonBottom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mButtonBottom.getText() == getResources().getString(R.string.T1_Ans2)){
+                    mButtonTop.setText(getResources().getString(R.string.T2_Ans1));
+                    mButtonBottom.setText(getResources().getString(R.string.T2_Ans2));
+                    mStoryTextView.setText(getResources().getString(R.string.T2_Story));
+                } else if (mButtonBottom.getText() == getResources().getString(R.string.T2_Ans2)) {
+                    mStoryTextView.setText(getResources().getString(R.string.T4_End));
+                    hideButtons();
+                } else if (mButtonBottom.getText() == getResources().getString(R.string.T3_Ans2)) {
+                    mStoryTextView.setText(getResources().getString(R.string.T5_End));
+                    hideButtons();
+                }
+            }
+        });
+    }
 
-
+    // Method to hide the buttons once you reach an ending to the story.
+    public void hideButtons(){
+        mButtonTop.setVisibility(View.GONE);
+        mButtonBottom.setVisibility(View.GONE);
     }
 }
